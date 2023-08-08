663d7a400bf07d2db44d97de64e9f1d04b27df15
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-08-08 16:53:58 +0200

    responses to one class

---
+++ src/main/java/ch15responsestooneclass/Book.java
@@ -0,0 +1,16 @@
+package ch15responsestooneclass;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Book {
+    String title;
+    String author;
+    String publisher;
+    Date publicationDate;
+    Date deliveryDate;
+}
+
+
+
