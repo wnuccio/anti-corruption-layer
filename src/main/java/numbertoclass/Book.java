d53abbfbce30cc75f9a1c9e1763ba2169560251e
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-08-12 19:36:08 +0200

    number to class

---
+++ src/main/java/numbertoclass/Book.java
@@ -0,0 +1,26 @@
+package numbertoclass;
+
+public class Book {
+    String title;
+    Price price;
+}
+
+class Price {
+    double value;
+
+    private Price(double value) {
+        this.value = value;
+    }
+
+    static Price euros(double value) {
+        return new Price(value);
+    }
+
+    public double euros() {
+        return value;
+    }
+
+    public Price discounted() {
+        return new Price(euros() * 0.90);
+    }
+}
