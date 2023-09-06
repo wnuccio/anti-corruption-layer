dc58058ac375ae59ae3b5b1a630d4e2d9015030b
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-09-06 11:47:40 +0200

    reorganize packages

---
+++ src/main/java/dtotoanemicclass/Book.java
@@ -0,0 +1,15 @@
+package dtotoanemicclass;
+
+import java.time.LocalDateTime;
+
+public class Book {
+    private String title;
+    private String publisher;
+    private String genre;
+    private Double price;
+    private LocalDateTime deliveryTime;
+    private String isbn;
+
+    // all getters/setters
+}
+
---
+++ src/main/java/identifiertoreference/Book.java
@@ -0,0 +1,40 @@
+package identifiertoreference;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+class DeliveredBooks {
+    List<DeliveredBook> deliveredBooks;
+
+    List<DeliveredBook> findAllBooksDeliveredTo(String personName) {
+        return deliveredBooks.stream()
+                .filter(deliveredBook -> deliveredBook.isDeliveredTo(personName))
+                .collect(Collectors.toList());
+    }
+}
+
+class DeliveredBook {
+    Book book;
+    Person person;
+
+    public boolean isDeliveredTo(String personName) {
+        return person.hasName(personName);
+    }
+}
+
+class Person {
+    String name;
+
+    String address;
+    public boolean hasName(String personName) {
+        return this.name.equals(personName);
+    }
+
+}
+class Book {
+    String title;
+}
+
+
+
+
