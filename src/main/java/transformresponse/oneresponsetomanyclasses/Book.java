6eefa3f8efe75d6d0989e4ada41347459e213339
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2023-09-26 16:17:53 +0200

    one response to many classes

---
+++ src/main/java/transformresponse/oneresponsetomanyclasses/Book.java
@@ -0,0 +1,17 @@
+package transformresponse.oneresponsetomanyclasses;
+
+import java.util.Date;
+
+class Book {
+    String title;
+    String author;
+}
+
+class BookPublication {
+    String title;
+    String publisher;
+    Date publicationDate;
+}
+
+
+
