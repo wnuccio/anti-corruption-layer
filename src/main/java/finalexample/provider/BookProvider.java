b4b7fcf347cdbe47031f1e372e55cd30f5a70027
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2024-07-10 16:09:51 +0200

    rename parameter

--- src/main/java/finalexample/provider/BookProvider.java
+++ src/main/java/finalexample/provider/BookProvider.java
@@ -3,7 +3,7 @@ package finalexample.provider;
 import finalexample.provider.dtos.BookBundleDto;
 
 public class BookProvider {
-    public BookBundleDto getBundle(String string) {
+    public BookBundleDto getBundle(String isbn) {
         // in a real application this class would be responsible for fetching the data from a remote service
         throw new UnsupportedOperationException("Not implemented");
     }
