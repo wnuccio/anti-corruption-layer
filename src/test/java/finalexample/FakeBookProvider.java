9ed788e1a1b164f102038d463d8bd0c04a181a94
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2024-08-22 16:10:23 +0200

    improve text fixture

--- src/test/java/finalexample/FakeBookProvider.java
+++ src/test/java/finalexample/FakeBookProvider.java
@@ -8,40 +8,38 @@ import finalexample.acl.dtos.PublisherDto;
 
 import java.util.List;
 
-import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toList;
 
 public class FakeBookProvider extends BookProvider {
+    public static final BookInfoDto BOOK_1 = new BookInfoDto("Refactoring", "Fowler", "978-1234567876", List.of("refactoring", "clean-code", "coding"));
+    public static final PublishedBookDto PUB_BOOK_1 = new PublishedBookDto("Refactoring", "978-1234567876", 10.00, 2002);
+
+    public static final BookInfoDto BOOK_2 = new BookInfoDto("Design Patterns", "Gof", "978-0201633610", List.of("design", "pattern", "coding"));
+    public static final PublishedBookDto PUB_BOOK_2 = new PublishedBookDto("Design Pattern", "978-0201633610", 30.00, 2000);
+
+    public static final BookInfoDto BOOK_3 = new BookInfoDto("Implementing DDD", "Vernon", "978-0201633603", List.of("design", "ddd"));
+    public static final PublishedBookDto PUB_BOOK_3 = new PublishedBookDto("Design Pattern", "978-0201633603", 40.00, 2003);
+
+    public static final BookInfoDto BOOK_4 = new BookInfoDto("Wrong Isbn", "Unknown", "xxxxxxxxxxxxx", "wrong-isbn");
+    public static final PublishedBookDto PUB_BOOK_4 = new PublishedBookDto("Wrong Isbn", "xxxxxxxxxxxxx", 30.00, 2000);
+
+    public static final BookInfoDto BOOK_5 = new BookInfoDto("Wrong Isbn", "Unknown", "978-0201633604", "not-referred-by-any-publisher");
+
+    public static final BookInfoDto BOOK_6 = new BookInfoDto("Invalid Price", "Unknown", "978-0201633605", "invalid-price");
+    public static final PublishedBookDto PUB_BOOK_6 = new PublishedBookDto("Invalid Price", "978-0201633605", -30.00, 2000);
+
+    public static final PublisherDto PUBLISHER_A = new PublisherDto("Addison-Wesley", "USA",
+            List.of(PUB_BOOK_1, PUB_BOOK_3));
+
+    public static final PublisherDto PUBLISHER_B = new PublisherDto("Pearson", "USA",
+            List.of(PUB_BOOK_2, PUB_BOOK_4, PUB_BOOK_6));
+
     private final List<BookInfoDto> bookInfos;
     private final List<PublisherDto> publishers;
 
     public FakeBookProvider() {
-        BookInfoDto book1 = new BookInfoDto("Refactoring", "Fowler", "978-1234567876",
-                List.of("refactoring", "clean-code", "coding"));
-        PublishedBookDto pubBook1 = new PublishedBookDto("Refactoring", "978-1234567876", 10.00, 2002);
-
-        BookInfoDto book2 = new BookInfoDto("Design Patterns", "Gof", "978-0201633610",
-                List.of("design", "pattern", "coding"));
-        PublishedBookDto pubBook2 = new PublishedBookDto("Design Pattern", "978-0201633610", 30.00, 2000);
-
-        BookInfoDto book3 = new BookInfoDto("Implementing DDD", "Vernon", "978-0201633603",
-                List.of("design", "ddd"));
-        PublishedBookDto pubBook3 = new PublishedBookDto("Design Pattern", "978-0201633603", 40.00, 2003);
-
-        BookInfoDto book4 = new BookInfoDto("Wrong Isbn", "Unknown", "xxxxxxxxxxxxx", "wrong-isbn");
-        PublishedBookDto pubBook4 = new PublishedBookDto("Wrong Isbn", "xxxxxxxxxxxxx", 30.00, 2000);
-
-        BookInfoDto book5 = new BookInfoDto("Wrong Isbn", "Unknown", "978-0201633604", "not-referred-by-any-publisher");
-
-        BookInfoDto book6 = new BookInfoDto("Invalid Price", "Unknown", "978-0201633605", "invalid-price");
-        PublishedBookDto pubBook6 = new PublishedBookDto("Invalid Price", "978-0201633605", -30.00, 2000);
-
-        PublisherDto publisher1 = new PublisherDto("Addison-Wesley", "USA", List.of(pubBook1, pubBook3));
-        PublisherDto publisher2 = new PublisherDto("Pearson", "USA", List.of(pubBook2, pubBook4, pubBook6));
-        PublisherDto publisher3 = new PublisherDto("O'Reilly", "USA", emptyList());
-
-        this.bookInfos = List.of(book1, book2, book3, book4, book5, book6);
-        this.publishers = List.of(publisher1, publisher2, publisher3);
+        this.bookInfos = List.of(BOOK_1, BOOK_2, BOOK_3, BOOK_4, BOOK_5, BOOK_6);
+        this.publishers = List.of(PUBLISHER_A, PUBLISHER_B);
     }
 
     @Override
