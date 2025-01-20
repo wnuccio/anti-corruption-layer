621182975db45e4b654aeeb71a5ed1c3854a389d
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2025-01-20 09:27:15 +0100

    shorten field name

--- src/main/java/finalexample/domain/BookUseCase.java
+++ src/main/java/finalexample/domain/BookUseCase.java
@@ -4,34 +4,34 @@ import java.util.List;
 import java.util.Optional;
 
 public class BookUseCase {
-    private final BookProvider bookProvider;
+    private final BookProvider provider;
 
     public BookUseCase(BookProvider bookProvider) {
-        this.bookProvider = bookProvider;
+        this.provider = bookProvider;
     }
 
     public Optional<Book> findBookByIsbn(Isbn isbn) {
-        BookBundle bundle = bookProvider.retrieveSoftwareBooks();
+        BookBundle bundle = provider.retrieveSoftwareBooks();
         return bundle.find(isbn);
     }
 
     public Optional<Book> findCheapestBook() {
-        BookBundle bundle = bookProvider.retrieveSoftwareBooks();
+        BookBundle bundle = provider.retrieveSoftwareBooks();
         return bundle.cheapestBook();
     }
 
     public List<String> findPublihsersOfBook(String title) {
-        BookBundle bundle = bookProvider.retrieveSoftwareBooks();
+        BookBundle bundle = provider.retrieveSoftwareBooks();
         return bundle.publishersOf(title);
     }
 
     public List<Book> findBooksLessExpensiveThan(Price price) {
-        BookBundle bundle = bookProvider.retrieveSoftwareBooks();
+        BookBundle bundle = provider.retrieveSoftwareBooks();
         return bundle.booksLessExpensiveThan(price);
     }
 
     public List<Book> findRecentlyPublishedBooks(int year) {
-        BookBundle bundle = bookProvider.retrieveSoftwareBooks();
+        BookBundle bundle = provider.retrieveSoftwareBooks();
         return bundle.booksPublishedAfter(year);
     }
 }
